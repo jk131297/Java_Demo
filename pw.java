b55ac1fd7e6f71b61e5409ed41e2d762876e4734
jk131297 <jk13121997@gmail.com>
2023-01-31 12:43:30 +0530

    new Changes

---
+++ pw.java
@@ -0,0 +1,23 @@
+public class pw{
+
+ public static void main(String[] args) {
+System.out.println(args[0] +" "+args[1]);
+    m1();
+}
+
+public static void m1(){
+System.out.println("Ile");
+// code
+}
+
+
+public void m2(){
+System.out.println("Ne fIle");
+// code
+}
+
+public void m3(){
+System.out.println("New fIle");
+// code
+}
+}
