91d3aa1573d830649cffca260a6661d006317c0e
jk131297 <jk13121997@gmail.com>
2023-02-08 12:07:45 +0530

    Remove constant from sentence

---
+++ RemoveConst.java
@@ -0,0 +1,29 @@
+public class RemoveConst {
+    
+    public static void main(String[] args){
+        deletConsonants("Hello, have a good day");
+    }
+
+    
+public static void deletConsonants(String sentence){
+    String vowels = "aeiou";
+    String[] strBlocksArr = sentence.split(" ");
+    String[] newStrBlock = new String[strBlocksArr.length];
+    String word = new String();
+    for(int i=0; i<strBlocksArr.length;i++){
+        word = strBlocksArr[i];
+        char[] charArr = word.toCharArray();
+        for(char c :charArr){
+            if(!(vowels.contains(Character.toString(c)) ||
+            vowels.toUpperCase().contains(Character.toString(c)) )){
+                word = word.replace(Character.toString(c), "");
+            }
+        }
+        newStrBlock[i]=word;
+    }
+    String finalSen = String.join(" ", newStrBlock);
+    System.out.println(finalSen);
+
+}
+
+}
